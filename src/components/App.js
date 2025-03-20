6ecc549e13c05f425f435128e726d013c8bb5171
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-03-20 17:02:01 +0530

    added personal greeting component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
+import { useState } from "react";
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  let [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <h2>Enter your name:</h2>
+      <input
+        type="text"
+        placeholder="Enter your name..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      {name && <p>Hello {name}!</p>}
+    </div>
+  );
+};
+
+export default App;
